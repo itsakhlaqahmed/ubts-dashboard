1d0488245fb6be4f333240b66574e2c518302201
Akhlaq Ahmed <akhlaqahmed990@gmail.com>
2024-08-16 21:15:46 +0500

    complete navbar-styles

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,27 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 import { FaBell } from "react-icons/fa6";
 
 const Navbar = ({ name }) => {
+  const [openNotificaitons, setOpenNotification] = useState(false);
+  const [openProfilePanel, setOpenProfilePanel] = useState(false);
   const userImage =
     "https://img.freepik.com/free-photo/horizontal-portrait-smiling-happy-young-pleasant-looking-female-wears-denim-shirt-stylish-glasses-with-straight-blonde-hair-expresses-positiveness-poses_176420-13176.jpg";
+
+  const onClickNotificationButton = () => {
+    setOpenNotification(!openNotificaitons);
+    alert(openNotificaitons);
+  };
+
+  const onClickProfile = () => {
+    setOpenProfilePanel(!openProfilePanel);
+    alert(openProfilePanel);
+  };
 
   return (
     <nav>
       <div className="header">
         <div className="nav-container">
+          <p className="nav-title">Dashboard</p>
           <ul>
             <li>
-              <button className="notification-icon-box" onClick={()=>{alert('click')}}>
-                <FaBell className="notificaion-icon"/>
+              <button
+                className="notification-icon-box"
+                onClick={onClickNotificationButton}
+              >
+                <FaBell className="notificaion-icon" />
               </button>
             </li>
             <li>
-              <div className="profile-pic">
+              <button className="profile-pic" onClick={onClickProfile}>
                 <img src={userImage} alt="Profile Picture" />
                 <p className="user-name">{name}</p>
-              </div>
+              </button>
             </li>
           </ul>
         </div>
