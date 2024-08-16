6ffe62e720b6cc311482f0dadca129f896427dd7
Akhlaq Ahmed <akhlaqahmed990@gmail.com>
2024-08-16 21:17:00 +0500

    add leftdrawer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import Navbar from "./navbar/Navbar";
+import LeftDrawer from "./left-drawer/LeftDrawer";
+import React, { Fr } from "react";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <LeftDrawer />
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
