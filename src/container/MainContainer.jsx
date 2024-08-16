28f825c170ae8e2d0af5da2429c37ba0192cee57
Akhlaq Ahmed <akhlaqahmed990@gmail.com>
2024-08-17 02:05:59 +0500

    add leftDrawer

--- src/container/MainContainer.jsx
+++ src/container/MainContainer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../navbar/Navbar";
+import LeftDrawer from "../left-drawer/LeftDrawer";
+import './main-container.css';
 
 const MainContainer = () => {
   return (
-    <div>MainContainer</div>
-  )
-}
+    <div className="main-container">  
+      <LeftDrawer />
+      <Navbar />
+    </div>
+  );
+};
 
-export default MainContainer
+export default MainContainer;
--- src/left-drawer/LeftDrawer.jsx
+++ src/left-drawer/LeftDrawer.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import "./leftdrawer.css";
+import { Link } from "react-router-dom";
+import { FaHouse, FaBusSimple, FaRoute } from "react-icons/fa6";
+import { IoIosPeople } from "react-icons/io";
 
 const LeftDrawer = () => {
   return (
-    <div>LeftDrawer</div>
-  )
-}
+    <div className="leftdrawer-container">
+      <div className="logo-container">
+        <div className="ubts-logo">UBTS</div>
+      </div>
 
-export default LeftDrawer
+      <div className="links-container">
+        <ul>
+          <li style={{background: '#ffffff2a',}}>
+            <Link to="/">
+              <FaHouse />
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <IoIosPeople />
+              Passengers
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <FaBusSimple />
+              Drivers
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <FaRoute />
+              Routes
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default LeftDrawer;
